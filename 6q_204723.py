a293a584c45effce37c0a0ff66a91acfc5939df8
ranapratap48 <97620087+ranapratap48@users.noreply.github.com>
2022-01-12 22:11:57 +0530

    Add files via upload

---
+++ 6q_204723.py
@@ -0,0 +1,6 @@
+dict_list = [{'name': 'affirm', 'confidence': 0.9448149204254}, {'name': 'affirm', 'confidence': 0.944814920425415},
+             {'name': 'inform', 'confidence': 0.9842240810394287}, {'name': 'inform', 'confidence': 0.9842240810394287}]
+
+res = list(map(dict, set(tuple(sorted(sub.items())) for sub in dict_list)))
+
+print(res)
